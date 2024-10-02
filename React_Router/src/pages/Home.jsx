0e9fc2dfd51f52783fc9c47017a9fa371cb6fa96
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-10-02 13:36:49 +0300

    Navigating between Pages with Links

--- React_Router/src/pages/Home.jsx
+++ React_Router/src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom"
+
 export default function HomePage(){
     return(
+        <>
         <h1>My Home Page!</h1>
+        <p>Go to <Link to="/products"> The List of Products</Link>.</p>
+        </>
     )
 }
