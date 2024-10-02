20cd25fce819a4aeb6b2e4040754c1b92b64e2d5
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-10-02 13:30:29 +0300

    Exploring an Alternative Way of Defining Routes

--- React_Router/src/App.jsx
+++ React_Router/src/App.jsx
@@ -1,7 +1,23 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // createRoutesFromElements,
+  // Route
+} from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 
+//First Way to defining Route:
+// const routeDefinintinos = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage />} />
+//     <Route path='/products' element={<ProductsPage />} />
+//   </Route>
+// )
+// const router = createBrowserRouter(routeDefinintinos);
+
+
+//Second Way to defining Route:
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
   { path: '/products', element: <ProductsPage /> }
