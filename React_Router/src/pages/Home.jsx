28e0eac1f62014f270b6b47455ffa71b0e7ca7c8
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-10-02 15:57:58 +0300

    Navigating Programmatically

--- React_Router/src/pages/Home.jsx
+++ React_Router/src/pages/Home.jsx
@@ -1,10 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+
+    //Navigating Programmatically => (as Link) :
+    const navigate = useNavigate();
+    function navigateHandler() {
+        navigate('/products');
+    }
+
     return (
         <>
             <h1>My Home Page!</h1>
-            <p>Go to <Link to="/products"> The List of Products</Link>.</p>
+            <p>
+                Go to <Link to="/products"> The List of Products</Link>.
+            </p>
+            <p>
+                <button onClick={navigateHandler}>Navigate</button>
+            </p>
         </>
     );
 }
