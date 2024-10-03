faa6ec85dcf33cc0ef3320e03da06f9f513dd94f
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-10-03 12:20:41 +0300

    6. Output a list of dummy events to the EventsPage //    Every list item should include a link to the respective EventDetailPage

--- React_Router_Project/src/pages/EventsPage.jsx
+++ React_Router_Project/src/pages/EventsPage.jsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
+
+
+const DUMMY_EVENTS = [
+    {
+        id: 'e1',
+        title: 'Some Event'
+    },
+    {
+        id: 'e2',
+        title: 'Another Event'
+    },
+]
+
+
 export default function EventsPage() {
     return (
         <>
             <h1>The Events Page</h1>
+            <ul>
+                {DUMMY_EVENTS.map(
+                    (event) => <li key={event.id}>
+                        <Link to={event.id}
+                        >
+                            {event.title}
+                        </Link>
+                    </li>
+                )}
+            </ul>
         </>
     );
 }
