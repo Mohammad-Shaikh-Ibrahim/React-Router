6aa96d0570b20f99378f40a2e362d7b850acfd14
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-10-03 12:06:09 +0300

    3. Add a root layout that adds the <MainNavigation> component above all page components

--- React_Router_Project/src/App.jsx
+++ React_Router_Project/src/App.jsx
@@ -26,13 +26,19 @@ import EventsPage from "./pages/EventsPage";
 import EventDetaiPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
+import RootLayoutPage from "./pages/RootLayout";
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/events', element: <EventsPage /> },
-  { path: '/events/:eventId', element: <EventDetaiPage /> },
-  { path: '/events/:new', element: <NewEventPage /> },
-  { path: '/events/:eventId/edit', element: <EditEventPage /> },
+  {
+    path: '/', element: <RootLayoutPage />, children: [
+      { index: true, element: <HomePage /> },
+      { path: 'events', element: <EventsPage /> },
+      { path: 'events/:eventId', element: <EventDetaiPage /> },
+      { path: 'events/:new', element: <NewEventPage /> },
+      { path: 'events/:eventId/edit', element: <EditEventPage /> },
+    ]
+  },
+
 ])
 
 function App() {
