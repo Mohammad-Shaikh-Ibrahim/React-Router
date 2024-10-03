2237adfc783e77b1b589f97a9851e7e3ff992ec1
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-10-03 12:24:44 +0300

    7. Output the ID of the selected event on the EventDetailPage

--- React_Router_Project/src/pages/EventDetailPage.jsx
+++ React_Router_Project/src/pages/EventDetailPage.jsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+
 export default function EventDetaiPage() {
+    const params = useParams();
+
     return (
         <>
             <h1>The EventsDetail Page</h1>
+            <p>Event Id: {params.eventId}</p>
         </>
     );
 }
