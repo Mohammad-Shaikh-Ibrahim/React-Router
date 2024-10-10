c7528d0a0872bee05b8897dadd70b9d66b994468
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-10-10 16:29:35 +0300

    Controlling Which Data Should Be Deferred

--- React_Router_Project/src/pages/EventDetailPage.jsx
+++ React_Router_Project/src/pages/EventDetailPage.jsx
@@ -1,23 +1,30 @@
 // Import necessary hooks and components
-import { useRouteLoaderData, json, redirect } from "react-router-dom";
+import { useRouteLoaderData, json, redirect, defer, Await } from "react-router-dom";
 import EventItem from '../components/EventItem';
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 export default function EventDetailPage() {
     // Correct use of useRouteLoaderData
-    const data = useRouteLoaderData('event-detail');
+    const { event, events } = useRouteLoaderData('event-detail');
 
     return (
         <>
-            {/* Render the event item */}
-            <EventItem event={data.event} />
+            <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+                <Await resolve={event}>
+                    {(lodedEvent) => <EventItem event={lodedEvent} />}
+                </Await>
+            </Suspense>
+            <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+                <Await resolve={events}>
+                    {(lodedEvents) => <EventsList event={lodedEvents} />}
+                </Await>
+            </Suspense>
         </>
     );
 }
 
-// Loader function to fetch event details
-export async function loader({ params }) {
-    const id = params.eventId;
-
+async function loadEvent(id) {
     // Fetch event details from the backend
     const response = await fetch('http://localhost:8081/events/' + id);
 
@@ -27,11 +34,43 @@ export async function loader({ params }) {
             { message: "Could Not Fetch Details For Selected Event!" },
             { status: 500 }
         );
+    } else {
+        const resData = await response.json();
+        return resData.event;
     }
 
-    // Return parsed JSON data
-    const data = await response.json();
-    return { event: data };
+}
+
+async function loadEvents() {
+    //in this function you can use any Browser api like : localStorge,...  
+    //but you cant use hooks like : useState,...
+
+    const response = await fetch('http://localhost:8081/events');
+
+    if (!response.ok) {
+        // return { isError: true, message: 'Could Not Fetch Events.' }
+        // throw new Response(JSON.stringify({ message: 'Could Not Fetch Events.' }, { status: 500, }))
+        throw json(
+            { message: 'Could Not Fetch Events.' },
+            { status: 500, });
+    } else {
+        // const resData = await response.json();
+        // const res = new Response('any data', { status: 201 });
+        // return res;
+        const resData = await response.json();
+        return resData.events;
+    }
+}
+
+
+// Loader function to fetch event details
+export async function loader({ params }) {
+    const id = params.eventId;
+
+    return defer({
+        event: await loadEvent(id),
+        events: loadEvents(),
+    })
 }
 
 // Action function to handle delete event
